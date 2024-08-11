1bb5b702db14786c0278f51a218c2bcf60cd5fc7
Belayneh <bela@localhost.localdomain>
2024-08-12 00:12:00 +0300

    updated

--- script.js
+++ script.js
@@ -10,29 +10,28 @@ menu.addEventListener("click", () => {
 });
 
 close.addEventListener("click", () => {
-    menu.style.display = "inline-block";
-    close.style.display = "none";
-    navbar.style.display = "none";
-  });
+  menu.style.display = "inline-block";
+  close.style.display = "none";
+  navbar.style.display = "none";
+});
 
 // for search form display
 
-let search = document.querySelector('#search-form');
-let closeSearch = document.querySelector('#close');
-let searchIcon = document.querySelector('#search-icon');
+let search = document.querySelector("#search-form");
+let closeSearch = document.querySelector("#close");
+let searchIcon = document.querySelector("#search-icon");
 
-searchIcon.addEventListener('click', () => {
-    search.style.top = "0";
+searchIcon.addEventListener("click", () => {
+  search.style.top = "0";
 });
 
-closeSearch.addEventListener('click', () => {
-    search.style.top = "-110%";
+closeSearch.addEventListener("click", () => {
+  search.style.top = "-110%";
 });
-
 
 // home slider
 
-let swiper = new Swiper(".home-slider", {
+var swiper = new Swiper(".home-slider", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
@@ -43,5 +42,44 @@ let swiper = new Swiper(".home-slider", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  loop:true,
+  loop: true,
 });
+
+// review slider
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+// loder gif
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
